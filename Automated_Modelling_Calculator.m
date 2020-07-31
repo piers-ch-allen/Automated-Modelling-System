b60b572b6f235149f0d1d84980c12bbc69d6749d
pxa395-admin <pxa395-admin@EPS-HS1C7S2.adf.bham.ac.uk>
2020-07-31 11:27:11 +0100

    added dataManip class

--- Automated_Modelling_Calculator.m
+++ Automated_Modelling_Calculator.m
@@ -2,14 +2,18 @@ function Automated_Modelling_Calculator(numIterations, realData)
 %%Function to provide the parent class of the Automated modelling process
 % Itdoes inital data calculation and then provides a framework for
 % rerunning the process until a convergence is met.
+% Excel file called "AbacusVariables.xlsx" required.  It contains all
+% variable definitions for the model parameters.  Check readme for how this
+% is to be defined.
 
 %% Initial Step
 %Gather the dataFile at its initialisation point
 output = MainMultiple(1);
 iterCount = 2;
+bestData = 1;
 
 %perform data changes based on outputs and sve over data file.
-convergence = dataManip(output);
+[convergence, bestData] = dataManip(output, realData, bestData);
 
 %% Start iteration loop for model convergence
 %iterate through a number of model creations and data changes until a level
@@ -19,9 +23,11 @@ while (iterCount < numIterations && convergence == false)
     output = MainMultiple(iterCount);
     
     %perform data manipulation on the dataset
-    convergence = dataManip(output);
+    [convergence, bestData] = dataManip(output, realData, bestData);
     
     %increase iteration nunmber
     iterCount = iterCount + 1;
 end
 
+%% Do something with the output results after iterations are completed 
+% or a convergence is met
---
+++ dataManip.m
@@ -0,0 +1,1 @@
+function [convergence, bestData] = dataManip(outputDataSet, realData, bestCurrData)
